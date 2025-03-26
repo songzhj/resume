c4af8ab9d4eaf12d05de97e83e2f066f91a31217
songzhj <songzj.biz@gmail.com>
2025-03-26 22:03:01 +0800

    chore: change css module 'localsConvention' to 'camelCaseOnly'

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,11 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  vite: {
+    css: {
+      modules: {
+        localsConvention: 'camelCaseOnly',
+      },
+    },
+  },
 });
