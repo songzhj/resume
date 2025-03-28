cb16295c3e9409328b360884cfd0b7b2a211b411
Zhijia Song <songzj.biz@gmail.com>
2025-03-28 16:40:25 +0800

    chore: add tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,5 +12,7 @@ export default defineConfig({
         localsConvention: 'camelCaseOnly',
       },
     },
+
+    plugins: [tailwindcss()],
   },
 });
